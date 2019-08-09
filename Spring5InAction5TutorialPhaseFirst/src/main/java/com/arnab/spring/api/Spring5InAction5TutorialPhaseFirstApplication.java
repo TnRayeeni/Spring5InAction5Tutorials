317764061d9d292a7d221a8868400685babcb9ea
Arnab Banerjee <arnab.ban09@gmail.com>
2019-08-09 20:04:20 +0530

    Addeded 1st project of Spring 5

---
+++ Spring5InAction5TutorialPhaseFirst/src/main/java/com/arnab/spring/api/Spring5InAction5TutorialPhaseFirstApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5InAction5TutorialPhaseFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5InAction5TutorialPhaseFirstApplication.class, args);
+	}
+
+}
