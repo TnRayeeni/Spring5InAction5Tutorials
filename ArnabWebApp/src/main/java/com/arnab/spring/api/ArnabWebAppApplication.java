4a36e57896905bc8d0679f96411745e91ee208c9
Arnab Banerjee <arnab.ban09@gmail.com>
2019-08-10 18:15:49 +0530

    Tutorial project for Spring Security from Telusko

---
+++ ArnabWebApp/src/main/java/com/arnab/spring/api/ArnabWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArnabWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArnabWebAppApplication.class, args);
+	}
+
+}
